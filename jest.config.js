681d7c5f4d392a11d6ecfca6d8df6090c62ff356
Salt <732237136@qq.com>
2022-09-24 17:37:37 +0800

    fix: jest配置文件修复

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@
  * @Author: Salt
  * @Date: 2022-09-04 23:18:21
  * @LastEditors: Salt
- * @LastEditTime: 2022-09-19 22:25:15
+ * @LastEditTime: 2022-09-24 17:36:56
  * @Description: 这个文件的功能
  * @FilePath: \salt-lib\jest.config.js
  */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
   testEnvironment: 'jsdom',
   transform: {
