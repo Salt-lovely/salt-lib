42416afb2153df7848252a7c34434f12730406b1
Salt <732237136@qq.com>
2022-09-24 19:15:49 +0800

    fix: 回退部分代码

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@
  * @Author: Salt
  * @Date: 2022-09-04 23:18:21
  * @LastEditors: Salt
- * @LastEditTime: 2022-09-24 17:36:56
+ * @LastEditTime: 2022-09-24 19:13:33
  * @Description: 这个文件的功能
  * @FilePath: \salt-lib\jest.config.js
  */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+module.exports = {
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
   testEnvironment: 'jsdom',
   transform: {
